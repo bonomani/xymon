df68271d67fd603a4c50ab527e43f77814b84474
Bonomani <8841264+bonomani@users.noreply.github.com>
2026-02-01 06:14:55 +0100

    cmake: fix preset usage (remove -S with presets)

--- ci/run/cmake-configure.sh
+++ ci/run/cmake-configure.sh
@@ -17,7 +17,7 @@ if [[ -z "${LOCALCLIENT:-}" ]]; then
   fi
 fi
 
-cmake --preset "${PRESET}" -S . \	
+cmake --preset "${PRESET}" \	
   -DENABLE_SSL="${ENABLE_SSL}" \
   -DENABLE_LDAP="${ENABLE_LDAP}" \
   -DXYMON_VARIANT="${VARIANT}" \
