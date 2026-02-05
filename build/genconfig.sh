6bc0fc13c1cf0ca8dbbf35023c3799e7502e9bc2
Bonomani <8841264+bonomani@users.noreply.github.com>
2026-02-05 13:32:20 +0100

    Write config.h atomically

--- build/genconfig.sh
+++ build/genconfig.sh
@@ -2,16 +2,20 @@
 
 # Simpler than autoconf, but it does what we need it to do right now.
 
-echo "/* This file is auto-generated */" >include/config.h
-echo "#ifndef __CONFIG_H__" >>include/config.h
-echo "#define __CONFIG_H__ 1" >>include/config.h
+umask 022
+tmpcfg="$(mktemp include/config.h.XXXXXX)" || exit 1
+trap 'rm -f "$tmpcfg"' EXIT
+
+echo "/* This file is auto-generated */" >"$tmpcfg"
+echo "#ifndef __CONFIG_H__" >>"$tmpcfg"
+echo "#define __CONFIG_H__ 1" >>"$tmpcfg"
 
 echo "Checking for socklen_t"
 $CC -c -o build/testfile.o $CFLAGS build/test-socklent.c 1>/dev/null 2>&1
 if test $? -eq 0; then
-	echo "#define HAVE_SOCKLEN_T 1" >>include/config.h
+		echo "#define HAVE_SOCKLEN_T 1" >>"$tmpcfg"
 else
-	echo "#undef HAVE_SOCKLEN_T" >>include/config.h
+	echo "#undef HAVE_SOCKLEN_T" >>"$tmpcfg"
 fi
 
 echo "Checking for snprintf"
@@ -19,12 +23,12 @@ $CC -c -o build/testfile.o $CFLAGS build/test-snprintf.c 1>/dev/null 2>&1
 if test $? -eq 0; then
 	$CC -o build/testfile $CFLAGS build/testfile.o 1>/dev/null 2>&1
 	if test $? -eq 0; then
-		echo "#define HAVE_SNPRINTF 1" >>include/config.h
+		echo "#define HAVE_SNPRINTF 1" >>"$tmpcfg"
 	else
-		echo "#undef HAVE_SNPRINTF" >>include/config.h
+		echo "#undef HAVE_SNPRINTF" >>"$tmpcfg"
 	fi
 else
-	echo "#undef HAVE_SNPRINTF" >>include/config.h
+	echo "#undef HAVE_SNPRINTF" >>"$tmpcfg"
 fi
 
 echo "Checking for vsnprintf"
@@ -32,68 +36,68 @@ $CC -c -o build/testfile.o $CFLAGS build/test-vsnprintf.c 1>/dev/null 2>&1
 if test $? -eq 0; then
 	$CC -o build/testfile $CFLAGS build/testfile.o 1>/dev/null 2>&1
 	if test $? -eq 0; then
-		echo "#define HAVE_VSNPRINTF 1" >>include/config.h
+		echo "#define HAVE_VSNPRINTF 1" >>"$tmpcfg"
 	else
-		echo "#undef HAVE_VSNPRINTF" >>include/config.h
+		echo "#undef HAVE_VSNPRINTF" >>"$tmpcfg"
 	fi
 else
-	echo "#undef HAVE_VSNPRINTF" >>include/config.h
+	echo "#undef HAVE_VSNPRINTF" >>"$tmpcfg"
 fi
 
 echo "Checking for rpc/rpcent.h"
 $CC -c -o build/testfile.o $CFLAGS build/test-rpcenth.c 1>/dev/null 2>&1
 if test $? -eq 0; then
-	echo "#define HAVE_RPCENT_H 1" >>include/config.h
+		echo "#define HAVE_RPCENT_H 1" >>"$tmpcfg"
 else
-	echo "#undef HAVE_RPCENT_H" >>include/config.h
+	echo "#undef HAVE_RPCENT_H" >>"$tmpcfg"
 fi
 
 echo "Checking for sys/select.h"
 $CC -c -o build/testfile.o $CFLAGS build/test-sysselecth.c 1>/dev/null 2>&1
 if test $? -eq 0; then
-	echo "#define HAVE_SYS_SELECT_H 1" >>include/config.h
+		echo "#define HAVE_SYS_SELECT_H 1" >>"$tmpcfg"
 else
-	echo "#undef HAVE_SYS_SELECT_H" >>include/config.h
+	echo "#undef HAVE_SYS_SELECT_H" >>"$tmpcfg"
 fi
 
 echo "Checking for u_int32_t typedef"
 $CC -c -o build/testfile.o $CFLAGS build/test-uint.c 1>/dev/null 2>&1
 if test $? -eq 0; then
-	echo "#define HAVE_UINT32_TYPEDEF 1" >>include/config.h
+		echo "#define HAVE_UINT32_TYPEDEF 1" >>"$tmpcfg"
 else
-	echo "#undef HAVE_UINT32_TYPEDEF" >>include/config.h
+	echo "#undef HAVE_UINT32_TYPEDEF" >>"$tmpcfg"
 fi
 
 echo "Checking for PATH_MAX definition"
 $CC -o build/testfile $CFLAGS build/test-pathmax.c 1>/dev/null 2>&1
-if test -x build/testfile; then ./build/testfile >>include/config.h; fi
+if test -x build/testfile; then ./build/testfile >>"$tmpcfg"; fi
 
 echo "Checking for SHUT_RD/WR/RDWR definitions"
 $CC -o build/testfile $CFLAGS build/test-shutdown.c 1>/dev/null 2>&1
-if test -x build/testfile; then ./build/testfile >>include/config.h; fi
+if test -x build/testfile; then ./build/testfile >>"$tmpcfg"; fi
 
 echo "Checking for strtoll()"
 $CC -c -o build/testfile.o $CFLAGS build/test-strtoll.c 1>/dev/null 2>&1
 if test $? -eq 0; then
-	echo "#define HAVE_STRTOLL_H 1" >>include/config.h
+		echo "#define HAVE_STRTOLL_H 1" >>"$tmpcfg"
 else
-	echo "#undef HAVE_STRTOLL_H" >>include/config.h
+	echo "#undef HAVE_STRTOLL_H" >>"$tmpcfg"
 fi
 
 echo "Checking for uname"
 $CC -c -o build/testfile.o $CFLAGS build/test-uname.c 1>/dev/null 2>&1
 if test $? -eq 0; then
-	echo "#define HAVE_UNAME 1" >>include/config.h
+		echo "#define HAVE_UNAME 1" >>"$tmpcfg"
 else
-	echo "#undef HAVE_UNAME" >>include/config.h
+	echo "#undef HAVE_UNAME" >>"$tmpcfg"
 fi
 
 echo "Checking for setenv"
 $CC -c -o build/testfile.o $CFLAGS build/test-setenv.c 1>/dev/null 2>&1
 if test $? -eq 0; then
-	echo "#define HAVE_SETENV 1" >>include/config.h
+		echo "#define HAVE_SETENV 1" >>"$tmpcfg"
 else
-	echo "#undef HAVE_SETENV" >>include/config.h
+	echo "#undef HAVE_SETENV" >>"$tmpcfg"
 fi
 
 
@@ -103,15 +107,17 @@ fi
 #if test $? -eq 0; then
 #	echo "#define HAVE_BINARY_TREE 1" >>include/config.h
 #else
-	echo "#undef HAVE_BINARY_TREE" >>include/config.h
+	echo "#undef HAVE_BINARY_TREE" >>"$tmpcfg"
 #fi
 
 
 
-echo "#endif" >>include/config.h
+echo "#endif" >>"$tmpcfg"
+
+mv -f "$tmpcfg" include/config.h
+trap - EXIT
 
 echo "config.h created"
 rm -f testfile.o testfile
 
 exit 0
-
