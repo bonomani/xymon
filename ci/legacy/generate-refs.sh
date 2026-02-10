5925a9c8d64f96f0604c9f611291c57a2a72f500
Bonomani <8841264+bonomani@users.noreply.github.com>
2026-02-10 16:30:05 +0100

    Revert refs root override

--- ci/legacy/generate-refs.sh
+++ ci/legacy/generate-refs.sh
@@ -8,11 +8,10 @@ VARIANT="server"
 REF_NAME="ref"
 KEYFILES_NAME="keyfiles.sha256"
 BUILD_TOOL=""
-REF_STAGE_ROOT=""
 
 usage() {
   cat <<'USAGE' >&2
-Usage: $0 --root ROOT --os OS [--build TOOL] [--variant VARIANT] [--topdir TOPDIR] [--ref-name NAME] [--keyfiles-name NAME] [--refs-root DIR]
+Usage: $0 --root ROOT --os OS [--build TOOL] [--variant VARIANT] [--topdir TOPDIR] [--ref-name NAME] [--keyfiles-name NAME]
 USAGE
   exit 1
 }
@@ -45,10 +44,6 @@ while [ $# -gt 0 ]; do
       ;;
     --build)
       BUILD_TOOL="${2:-}"
-      shift 2
-      ;;
-    --refs-root)
-      REF_STAGE_ROOT="${2:-}"
       shift 2
       ;;
     *)
