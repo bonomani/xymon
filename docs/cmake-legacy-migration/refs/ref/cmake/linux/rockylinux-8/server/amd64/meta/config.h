0d5af280648aa790b948129b382f9804fe9248d9
bonomani <bonomani@users.noreply.github.com>
2026-03-10 12:50:46 +0100

    Update legacy reference artifacts (#42)
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

---
+++ docs/cmake-legacy-migration/refs/ref/cmake/linux/rockylinux-8/server/amd64/meta/config.h
@@ -0,0 +1,27 @@
+/* This file is auto-generated */
+#ifndef __CONFIG_H__
+#define __CONFIG_H__ 1
+
+#define PACKAGE_NAME "xymon"
+#define PACKAGE_VERSION "cmake"
+
+#define HAVE_SOCKLEN_T 1
+#define HAVE_SNPRINTF 1
+#define HAVE_VSNPRINTF 1
+/* #undef HAVE_RPCENT_H */
+#define HAVE_SYS_SELECT_H 1
+#define HAVE_UINT32_TYPEDEF 1
+#define HAVE_UNAME 1
+#define HAVE_SETENV 1
+#define HAVE_STRTOLL_H 1
+#define HAVE_BINARY_TREE 0
+
+#define XYMONTOPDIR "/var/lib/xymon"
+#define XYMONHOME "/var/lib/xymon/server"
+#define XYMONCLIENTHOME "/var/lib/xymon/client"
+#define XYMONLOGDIR "/var/log/xymon"
+#define XYMONHOSTNAME "954b2572fbc2"
+#define XYMONHOSTIP "127.0.0.1"
+#define XYMONHOSTOS "linux"
+
+#endif
